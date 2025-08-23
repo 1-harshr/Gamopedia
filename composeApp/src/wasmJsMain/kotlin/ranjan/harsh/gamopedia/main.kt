fdc60b6c1ea47d0f8fa35e73ff32444486fa860d
harsh.ranjan <harsh.ranjan@travenues.com>
2025-08-23 23:11:54 +0530

    feat: Initialize Koin for WasmJS and update dependencies

--- composeApp/src/wasmJsMain/kotlin/ranjan/harsh/gamopedia/main.kt
+++ composeApp/src/wasmJsMain/kotlin/ranjan/harsh/gamopedia/main.kt
@@ -3,10 +3,13 @@ package ranjan.harsh.gamopedia
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
 import kotlinx.browser.document
+import org.koin.core.context.startKoin
+import ranjan.harsh.gamopedia.di.initKoin
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
     ComposeViewport(document.body!!) {
+        initKoin ()
         App()
     }
 }
